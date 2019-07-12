74005a3571cdc138e95b955023d61ff0845062c5
gtarsia <guidotarsia@gmail.com>
2019-07-12 16:54:12 -0300

    Separate controls from chicken logic

--- entities/Chicken.gd
+++ entities/Chicken.gd
@@ -6,6 +6,10 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 export var speed = 20  # How fast the player will move (pixels/sec).
+var right = false
+var left = false
+var up = false
+var down = false
 var screen_size  # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
@@ -13,14 +17,27 @@ func _ready():
   screen_size = get_viewport_rect().size
 
 func _process(delta):
-  var velocity = Vector2()  # The player's movement vector.
+  up = false
+  down = false
+  left = false
+  right = false
   if Input.is_action_pressed("ui_right"):
-    velocity.x += 1
-  if Input.is_action_pressed("ui_left"):
-    velocity.x -= 1
+    right = true
+  elif Input.is_action_pressed("ui_left"):
+    left = true
   if Input.is_action_pressed("ui_down"):
-    velocity.y += 1
+    down = true
   if Input.is_action_pressed("ui_up"):
+    up = true
+
+  var velocity = Vector2()  # The player's movement vector.
+  if right:
+    velocity.x += 1
+  if left:
+    velocity.x -= 1
+  if down:
+    velocity.y += 1
+  if up:
     velocity.y -= 1
   if velocity.length() > 0:
     velocity = velocity.normalized() * speed
