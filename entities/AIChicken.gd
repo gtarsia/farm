0cc63a94b9f653fb1783fb5a0d1aa0ef655ff735
gtarsia <guidotarsia@gmail.com>
2019-07-12 18:32:38 -0300

    Add movement timers to chicken

--- entities/AIChicken.gd
+++ entities/AIChicken.gd
@@ -4,5 +4,15 @@ enum AIChickenState {IDLE, MOVING}
 var state = AIChickenState.IDLE
 
 func _process(delta):
-#  print(state)
+  print(state)
   pass
+
+func _on_StartMoveTimer_timeout():
+  state = AIChickenState.MOVING
+  $MovingTimer.start()
+
+func _on_MovingTimer_timeout():
+  state = AIChickenState.IDLE
+  $StartMoveTimer.start()
+  pass # Replace with function body.
+
