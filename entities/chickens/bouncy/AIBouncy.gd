0f15d55eceed03bec94d358d8055cad241dd4e2e
gtarsia <guidotarsia@gmail.com>
2019-07-23 15:08:27 -0300

    Add AIBouncy

---
+++ entities/chickens/bouncy/AIBouncy.gd
@@ -0,0 +1,20 @@
+extends Chicken
+
+export var force = 300
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#  pass
+
+func _on_AIBouncy_body_entered(body: RigidBody2D):
+  var direction = body.position - self.position
+  body.apply_central_impulse(direction.normalized() * force)
+  pass # Replace with function body.
